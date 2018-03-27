a8e63d0ab5e8cc8ac185f201c7635560431a4284
Deep Dhillon <deep@deepdhillon.ca>
2018-03-27 11:55:14 -0400

    added vendor folder and created project structure

--- cmd/wio/commands/create/create.go
+++ cmd/wio/commands/create/create.go
@@ -9,18 +9,53 @@ package create
 
 import (
     . "wio/cmd/wio/types"
+    . "github.com/logrusorgru/aurora"
+    "fmt"
+    "path/filepath"
 )
+
+// Interface for project types and how they are created
+type Type interface {
+    createStructure() (error)
+    printProjectStructure()
+    createTemplateProject() (error)
+    printNextCommands()
+}
 
 // Executes the create command provided configuration packet
 func Execute(config ConfigCreate) {
+    fmt.Print(Brown("Project Name: "))
+    fmt.Println(Cyan(filepath.Base(config.Directory)))
+    fmt.Print(Brown("Project Type: "))
+    fmt.Println(Cyan(config.AppType))
+    fmt.Print(Brown("Project Path: "))
+    fmt.Println(Cyan(config.Directory))
+    fmt.Println()
 
-    // create a directory
-    // create common project structure
-    // move common template files
+    var createType Type = App{config:config}
 
-    if config.AppType == "app" {
-        executeApp(config)
+    if config.AppType == "lib" {
+        createType = Lib{config:config}
+    }
+
+    fmt.Print(Brown("Creating project structure ..."))
+    err := createType.createStructure()
+
+    if err != nil {
+        fmt.Println(Red(" [failure]"))
     } else {
-        executeLib(config)
+        createType.printProjectStructure()
+    }
+
+    err = createType.createStructure()
+
+    if err != nil {
+        fmt.Println(Red(" [failure]"))
+        fmt.Println(Brown("Project creation failed!"))
+    } else {
+        fmt.Println()
+        fmt.Println(Brown("Project has been successfully created and initialized!!"))
+        fmt.Println(Brown("Check following commands: "))
+        createType.printNextCommands()
     }
 }
--- cmd/wio/commands/create/lib.go
+++ cmd/wio/commands/create/lib.go
@@ -8,12 +8,59 @@
 package create
 
 import (
+    "path/filepath"
+    "os"
     "fmt"
+
     . "wio/cmd/wio/types"
+    . "github.com/logrusorgru/aurora"
 )
 
-// Executes lib sub command of create command
-func executeLib(config ConfigCreate) {
-    fmt.Println(config)
+type Lib struct {
+    config ConfigCreate
 }
 
+// Creates project structure for library type
+func (lib Lib) createStructure() (error) {
+    srcPath := lib.config.Directory + string(filepath.Separator) + "src"
+    libPath := lib.config.Directory + string(filepath.Separator) + "lib"
+    testPath := lib.config.Directory + string(filepath.Separator) + "test"
+
+    err := os.MkdirAll(srcPath, os.ModePerm)
+    if err != nil {
+        return err
+    }
+
+    err = os.MkdirAll(libPath, os.ModePerm)
+    if err != nil {
+        return err
+    }
+
+    err = os.MkdirAll(testPath, os.ModePerm)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+// Prints the project structure for library type
+func (lib Lib) printProjectStructure() {
+    fmt.Println()
+    fmt.Println(Cyan("src    - put your source files here."))
+    fmt.Println(Cyan("lib    - libraries for the project go here."))
+    fmt.Println(Cyan("test   - put your files for unit testing here."))
+}
+
+// Creates a template project that is ready to build and upload for library type
+func (lib Lib) createTemplateProject() (error) {
+    return nil
+}
+
+// Prints all the commands relevant to library type
+func (lib Lib) printNextCommands() {
+    fmt.Println(Cyan("`wio build -h`"))
+    fmt.Println(Cyan("`wio run -h`"))
+    fmt.Println(Cyan("`wio upload -h`"))
+    fmt.Println(Cyan("`wio test -h`"))
+}
