ef0116eedd23b98a70cbcea7510b86d8791bf282
Deep Dhillon <deep@deepdhillon.ca>
2019-02-21 21:59:27 -0500

    [HF] fixed release bugs (#131)
    * fixed release bugs
    
    * support previous versions
    
    * arm arch fix

--- deploy/npm/postinstall.js
+++ deploy/npm/postinstall.js
@@ -1,22 +1,9 @@
 
-const path = require('path')
-const fs = require('fs')
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
 
-// Mapping from Node's `process.arch` to Golang's `$GOARCH`
-const ARCH_MAPPING = {
-    "ia32": "32-bit",
-    "x64": "64-bit",
-    "arm": "arm"
-};
-
-// Mapping between Node's `process.platform` to Golang's 
-const PLATFORM_MAPPING = {
-    "darwin": "darwin",
-    "linux": "linux",
-    "win32": "windows",
-    "freebsd": "freebsd"
-};
-
+const INVALID_INPUT = "Invalid inputs";
 function parsePackageJson() {
     const packageJsonPath = path.join(".", "package.json");
     if (!fs.existsSync(packageJsonPath)) {
@@ -27,38 +14,11 @@ function parsePackageJson() {
 
     let packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
 
-    // We have validated the config. It exists in all its glory
-    let binName = packageJson.goBinary.name;
-    let binPath = packageJson.goBinary.path;
-    let url = packageJson.goBinary.url;
-    let version = packageJson.version;
-    if (version[0] === 'v') version = version.substr(1);  // strip the 'v' if necessary v0.0.1 => 0.0.1
-
-    // Binary name on Windows has .exe suffix
-    if (process.platform === "win32") {
-        binName += ".exe"
-    }
-
-    // Interpolate variables in URL, if necessary
-    url = url.replace(/{{arch}}/g, ARCH_MAPPING[process.arch]);
-    url = url.replace(/{{platform}}/g, PLATFORM_MAPPING[process.platform]);
-    url = url.replace(/{{version}}/g, version);
-
-    if (process.platform === "win32") {
-        url = url.replace(/{{format}}/g, "zip");
-    } else {
-        url = url.replace(/{{format}}/g, "tar.gz");
-    }
-
-    url = url.replace(/{{bin_name}}/g, binName);
-    url = url.replace(/{{bin_name}}/g, binName);
-
-
     return {
-        binName: binName,
-        binPath: binPath,
-        url: url,
-        version: version
+        binName: packageJson.goBinary.name,
+        binPath: packageJson.goBinary.path,
+        url: "",
+        version: ""
     }
 }
 
@@ -66,8 +26,8 @@ function rename(callback) {
     let opts = parsePackageJson();
     if (!opts) return callback(INVALID_INPUT);
 
-    if (process.platform === "win32") {
-        fs.rename(opts.binPath + '/wio', opts.binPath + '/wio.exe', function(err) {
+    if (os.platform() === "win32") {
+        fs.rename(opts.binPath + '/' + opts.binName, opts.binPath + '/wio.exe', function(err) {
             if ( err ) console.log('ERROR: ' + err);
         });
     }
@@ -78,4 +38,4 @@ var myCallback = function (data) {
     process.exit(1)
 };
 
-rename(myCallback)
+rename(myCallback);
